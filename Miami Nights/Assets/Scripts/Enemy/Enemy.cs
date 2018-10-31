56c8b6272d737def4bd2adb39ba41a6c6f9f7b99
DaNuker96 <44637618+DaNuker96@users.noreply.github.com>
2018-10-31 12:42:32 +0000

    First Version

---
+++ Miami Nights/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 100f;
+    public GameObject deathParticle;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0f)
+        {
+            Die ();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+
+        Instantiate(deathParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
